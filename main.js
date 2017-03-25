8f37c0b108cee2e26c614629610e3aa1c6da0d1c
Misael Rojas <takahashi.misa@gmail.com>
2017-03-25 13:20:07 -0600

    clase 3

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-// fichero principal
-// importar librerias ES5 estilo ES6
+
 import * as angular from 'angular'
 import * as angularMaterial from 'angular-material'
+/*
+ import * as toolbar from 'componentes/toolbar/toolbar'
+ import * as sidenav from 'componentes/sidenav/sidenav'
+ import * as listaTareas from 'componentes/lista-tareas/lista-tareas'
+ import * as fichaTarea from 'componentes/ficha-tarea/ficha-tarea'
+ import * as fab from 'componentes/fab/fab'
+ */
+import portada from 'componentes/portada/portada'
 
-const appName = 'frontEdge';
+const nombreApp = 'frontEdge'
+//angular.module(nombreApp,['ngMaterial', toolbar.nombre, sidenav.nombre, listaTareas.nombre, fichaTarea.nombre, fab.nombre])
+angular.module(nombreApp,['ngMaterial', portada])
 
-// declaración del módulo principal y sus dependencias
-angular.
-module(appName, ['ngMaterial']);
+let documentoHTML = angular.element(document)
 
-// arranque manual de AngularJS
-// uso de las arrow functions de ES6
-angular.
-element(document).
-ready(() => angular.bootstrap(document, [appName]));
+let iniciarApp = ()=>angular.bootstrap(documentoHTML,[nombreApp])
+
+documentoHTML.ready(iniciarApp)

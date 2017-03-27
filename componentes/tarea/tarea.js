672d77051320789956c157a62656b2fb422458b1
Misael Rojas <takahashi.misa@gmail.com>
2017-03-27 09:32:53 -0600

    Video 4

---
+++ componentes/tarea/tarea.js
@@ -0,0 +1,22 @@
+import * as angular from 'angular'
+import * as angularMaterial from 'angular-material'
+import * as angularUiRouter from 'angular-ui-router'
+
+const raiz ="./componentes/"
+const nombreComponente = 'tarea'
+
+let configurarEstado = ($stateProvider) => {
+    $stateProvider
+        .state(nombreComponente, {
+            url: `/${nombreComponente}`,
+            template: `<${nombreComponente}></${nombreComponente}>`
+        });
+}
+
+angular.module(nombreComponente, ['ngMaterial','ui.router'])
+    .component(nombreComponente, {
+        templateUrl: `${raiz}${nombreComponente}/${nombreComponente}.html`
+    })
+    .config(configurarEstado)
+
+export default nombreComponente
